8ef775a511ba12f2d877a3973a3579609a1ecd69
Mohamed Magdy <mohamed.magdy@sumup.com>
2014-08-17 23:44:50 +0200

    #12 Adding comments to the code

--- lib/event_publisher/controllers/trackable.rb
+++ lib/event_publisher/controllers/trackable.rb
@@ -1,5 +1,8 @@
 module EventPublisher
   module Trackable
+    # This method logs the user's event.
+    # Usage:
+    #   track_event(current_user, "Logged in")
     def track_event(user, event)
       user ||= current_event_publisher_user || current_anonymous_user
 
@@ -8,21 +11,14 @@ module EventPublisher
       user.track_event(event) # Creating the event
     end
 
-    # A method used to create an anonymous user to attach
-    # the events to.
-    def current_anonymous_user
-      EventPublisher::AnonymousUser.find_by_id(cookies.signed[:event_publisher_user_id]) ||
-      EventPublisher::AnonymousUser.create
-    end
-
-    def set_cookies_for(user)
-      # Save the user ID in a signed cookie so no one can tweak it
-      cookies.signed[:event_publisher_user_id] = user.id
-      cookies.signed[:event_publisher_user_type] = user.class.name
-    end
-
+    # This method is called after the user logged in to migrate
+    # the events stored before logging in (as anonymous user).
+    # Usage:
+    #   def after_login_path(resource)
+    #     migrate_events_after_login(resource)
+    #   end
     def migrate_events_after_login(user)
-      previously_logged_in = self.current_event_publisher_user
+      previously_logged_in = current_event_publisher_user
 
       # Moving the events of the previosuly logged in user to the current user
       previously_logged_in.event_trackings.update_all(
@@ -34,6 +30,27 @@ module EventPublisher
       set_cookies_for(user)
     end
 
+    private
+    # A method used to create an anonymous user to attach
+    # the events to.
+    def current_anonymous_user
+      EventPublisher::AnonymousUser.find_by_id(cookies.signed[:event_publisher_user_id]) ||
+      EventPublisher::AnonymousUser.create
+    end
+
+    # Sets the event_publisher cookies. The cookies are:
+    #   event_publisher_user_id: stores the user's id
+    #   event_publisher_user_type: stores the user type. For exaple AnonymousUser or User
+    def set_cookies_for(user)
+      # Save the user ID in a signed cookie so no one can tweak it
+      cookies.signed[:event_publisher_user_id] = user.id
+      cookies.signed[:event_publisher_user_type] = user.class.name
+    end
+
+    # Returns the current_user for event_publisher.
+    # It's different from current_user where it finds
+    # the user using the cookies event_publisher_user_type
+    # and event_publisher_user_id
     def current_event_publisher_user
       klass = cookies.signed[:event_publisher_user_type]
       user_id = cookies.signed[:event_publisher_user_id]
--- lib/event_publisher/models/trackable.rb
+++ lib/event_publisher/models/trackable.rb
@@ -9,6 +9,9 @@ module EventPublisher
   end
 
   module InstanceMethods
+    # Tracks an event for a user.
+    # Usage:
+    #   current_user.track_event("Logged in")
     def track_event(event)
       event = Event.where(name: event).first_or_create if event.is_a?(String)
       self.event_trackings.create(event_id: event.id)
