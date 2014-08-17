ea11d905ac5b7205f0c15799606d9a5c68804858
Mohamed Magdy <mohamed.magdy@sumup.com>
2014-08-18 00:37:54 +0200

    Calling attr_protected if defined

--- app/models/event_publisher/event.rb
+++ app/models/event_publisher/event.rb
@@ -1,5 +1,7 @@
 module EventPublisher
   class Event < ActiveRecord::Base
+    attr_protected if defined?(attr_protected)
+
     # Relations
     has_many :event_trackings
   end
--- app/models/event_publisher/event_tracking.rb
+++ app/models/event_publisher/event_tracking.rb
@@ -1,5 +1,7 @@
 module EventPublisher
   class EventTracking < ActiveRecord::Base
+    attr_protected if defined?(attr_protected)
+
     # Relations
     belongs_to :event
     belongs_to :trackable, polymorphic: true
