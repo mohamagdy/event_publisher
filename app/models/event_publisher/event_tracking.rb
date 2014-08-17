f1b536fdbc74e139cdbd9be01f0e706050789877
Mohamed Magdy <mohamed.magdy@sumup.com>
2014-08-18 00:25:08 +0200

    Removing attr_accessible from event_tracking model

--- app/models/event_publisher/event_tracking.rb
+++ app/models/event_publisher/event_tracking.rb
@@ -1,8 +1,5 @@
 module EventPublisher
   class EventTracking < ActiveRecord::Base
-    # Accessible attributes
-    attr_accessible :event_id, :ip, :operation_system, :trackable_id, :trackable_type
-
     # Relations
     belongs_to :event
     belongs_to :trackable, polymorphic: true
